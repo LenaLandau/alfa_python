b952a1e0d91e146f40e6e0f6a4119c87182989fc
JSBonn <jan.speller@t-online.de>
2024-05-10 12:30:05 +0200

    Jan

--- server/server_full.py
+++ server/server_full.py
@@ -19,6 +19,16 @@ def hello_param():
 		return "MISSING param 'query'\n"
 	return query*2
 
+
+@app.route('/newtryJ')
+def floor_function(value,float)->int:
+	if value < 0:
+		return int(value) - 1
+	else:
+		return int(value)
+	
+	
+
 if __name__ == '__main__':
 	print("Running on http://0.0.0.0:5000")
 	app.run(debug=True, host='0.0.0.0', port=5000)
