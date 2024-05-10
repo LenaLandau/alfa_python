1d915ad0c97bc7e4f7f69448867b101523497db2
pannous <info@pannous.com>
2024-05-10 13:38:54 +0200

    server pure

--- server/server.py
+++ server/server.py
@@ -16,9 +16,26 @@ def hallo():
 def weekend_param():
 	return "Bald ist Wochenende!!"
 
+@app.route('/TGIF')
+def Idea():
+	return "Wir sollen am WE feiern gehen"
+
+
 @app.route("/python")
 def python_kurs():
-    return "Wir lernen Python"
+	return "Wir lernen Python"
+
+@app.route('/newtryJ')
+def floor_function(): # fixed by K. ;)
+	if 'value' in flask.request.args:
+		value = int(flask.request.args['value'])
+	else:
+		return "MISSING param 'value'\n"
+	if value < 0:
+		return int(value) - 1
+	else:
+		return int(value)
+
 
 if __name__ == '__main__':
 	print("Running on http://0.0.0.0:8080")
